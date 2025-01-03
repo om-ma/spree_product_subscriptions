b77ae0cebcfc31472a5cb89c05f3815dafec2467
OmSolutionz <omsolutionpk@gmail.com>
2025-01-03 19:47:16 +0500

    feat: integration stripe subscription in yummatcha

--- app/overrides/add_subscribable_fields_to_products_show_page.rb
+++ app/overrides/add_subscribable_fields_to_products_show_page.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: "spree/products/_cart_form",
   name: "add_subscribable_fields_to_products_show",
-  insert_before: ".add-to-cart",
+ insert_after: ".qty-wrap",
   partial: "spree/products/subscription_fields"
 )
